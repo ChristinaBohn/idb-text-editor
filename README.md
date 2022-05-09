ccaaa758c42e3f6120740824457fa3077cdb4f5d
Christina Bohn <christinajbohn@gmail.com>
2022-05-09 05:10:59 -0700

    Updated Readme, deploying to Heroku.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# idb-text-editor
+# IDB Text Editor
 IDB Text Editor is a single-page text editor which runs in the browser, uses the idb package, and meets PWA requirements.
+
+## User Story
+
+AS A developer
+I WANT to create notes or code snippets with or without an internet connection
+SO THAT I can reliably retrieve them for later use
+
+## Technologies Used
+
+    * Javascript
+    * Express
+    * Node.js
+    * IndexedDB
+    * Webpack
+    * Wepack PWA Manifest
+    * Service Workers
+
+# Links
+
+GitHub Repo:
+    * https://github.com/ChristinaBohn/idb-text-editor
+
+Heroku:
+    * 
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,47 +12,42 @@ const initdb = async () =>
     },
   });
 
-// Done: Add logic to a method that accepts some content and adds it to the database *** Renamed putDB to postDB
-export const postDb = async (content) => {
-  console.error('postDb not implemented');
-
-  // Create a connection to the database
-  const jateDb = await openDB('jate', 1);
-
-  // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readwrite');
-
-  // Open the desired object store.
-  const store = tx.objectStore('jate');
-
-  // Use the .add() method on the store and pass in the content.
-  const request = store.add({ jate: content });
-
-  // Get confirmation of the request.
-  const result = await request;
-  console.log('🚀 - data saved to the database', result);
-};
 
 // Done: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-  console.error('getDb not implemented');
+export const getDb = async (content) => {
 
-  // Create a connection to the database database and version we want to use.
-  const jateDb = await openDB('jate', 1);
+  console.log('GET from the database');
 
-  // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readonly');
+  const notesDb = await openDB('jate', 1);
 
-  // Open up the desired object store.
+  const tx = notesDb.transaction('jate', 'readonly');
+
   const store = tx.objectStore('jate');
 
-  // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
 
-   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
+  console.log('Data saved to the database', result);
   return result;
+}
+
+// Done: Add logic to a method that accepts some content and adds it to the database *** Renamed putDB to postDB
+export const putDb = async (id, content) => {
+
+  console.log('Post to the database');
+
+  const notesDb = await openDB('jate', 1);
+
+  const tx = notesDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({id: id, content: content});
+
+  const result = await request;
+
+  console.log('🚀 - data saved to the database', result);
+
 };
 
 initdb();
